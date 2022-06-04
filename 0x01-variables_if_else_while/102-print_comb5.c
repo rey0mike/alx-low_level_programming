7a672682ff1b9c30af8579d50118fa39e91ab506
rey0mike <mikiyasyesgat@gmail.com>
2022-06-04 05:57:46 +0300

    variables if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,37 +7,27 @@
 
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+        int i, j;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
-
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
+        for (i = 0; i <= 99; i++)
+        {
+                for (j = 0; j <= 99; j++)
+                {
+                        if (i < j && i != j)
+                        {
+                                putchar((i / 10) + '0');
+                                putchar((i % 10) + '0');
+                                putchar(' ');
+                                putchar((j / 10) + '0');
+                                putchar((j % 10) + '0');
+                                if (i != 98 || j != 99)
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                }
+        }
+        putchar('\n');
+        return (0);
 }
