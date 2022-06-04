b3593e3b926f8becce50a4c735fd618b7e37ec04
rey0mike <mikiyasyesgat@gmail.com>
2022-06-04 06:02:51 +0300

    variables if else while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,26 @@
 
 int main(void)
 {
-        int i, j;
-
-        for (i = 0; i <= 99; i++)
+    int i, j;
+    for (i = 0; i <= 99; i++)
+    {
+        for (j = 0; j <= 99; j++)
         {
-                for (j = 0; j <= 99; j++)
+            if (i < j && i != j)
+            {
+                putchar((i / 10) + '0');
+                putchar((i % 10) + '0');
+                putchar(' ');
+                putchar((j / 10) + '0');
+                putchar((j % 10) + '0');
+                if (i != 98 || j != 99)
                 {
-                        if (i < j && i != j)
-                        {
-                                putchar((i / 10) + '0');
-                                putchar((i % 10) + '0');
-                                putchar(' ');
-                                putchar((j / 10) + '0');
-                                putchar((j % 10) + '0');
-                                if (i != 98 || j != 99)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
+                    putchar(',');
+                    putchar(' ');
                 }
+            }
         }
-        putchar('\n');
-        return (0);
+    }
+    putchar('\n');
+    return (0);
 }
