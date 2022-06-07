8b5699b904ac74b3dc4baa7bab26d26fc7f1a81e
rey0mike <mikiyasyesgat@gmail.com>
2022-06-07 17:04:26 +0300

    c coding try out

---
+++ 0x02-functions_nested_loops/0-puctchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print _putchar, followed by new line
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf('_');
+	printf('p');
+	printf('u');
+	printf('t');
+	printf('c');
+	printf('h');
+	printf('a');
+	printf('r');
+	printf('\n');
+
+	return (0);
+}
