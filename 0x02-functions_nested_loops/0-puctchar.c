c217ce781dd36677813e0fdf8e4a38467b1af521
rey0mike <mikiyasyesgat@gmail.com>
2022-06-07 17:14:50 +0300

    changes

--- 0x02-functions_nested_loops/0-puctchar.c
+++ 0x02-functions_nested_loops/0-puctchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - print _putchar, followed by new line
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	printf('_');
-	printf('p');
-	printf('u');
-	printf('t');
-	printf('c');
-	printf('h');
-	printf('a');
-	printf('r');
-	printf('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
