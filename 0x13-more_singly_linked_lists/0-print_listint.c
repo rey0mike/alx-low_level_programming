85e96ce8663298487c9ffafed75c74fd79779caa
rey0mike <mikiyasyesgat@gmail.com>
2022-07-19 18:28:25 +0300

    alx-low_level_programming

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,16 @@
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
- * @h: A pointer to the head of the list_t list.
+ * @h: A pointer to the head of the ltint_t *h)
+{
+	size_t nodes = 0;
+	while (h)
+	{
+		nist_t list.
  *
  * Return: The number of nodes in the list_t list.
  */
-size_t print_listint(const listint_t *h)
-{
-	size_t nodes = 0;
-
-	while (h)
-	{
-		nodes++;
+size_t print_listint(const lisodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
